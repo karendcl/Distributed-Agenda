b5e3fee10fd180c2045807367645117419599c68
Loitzel <loitzelmorales@gmail.com>
2024-05-11 16:49:30 -0400

    chore: Refactor database creation and loading logic

--- src/back/DB/DB.py
+++ src/back/DB/DB.py
@@ -1,14 +1,27 @@
-from back.DB.DB_Utils import DB_Utils
+from typing import List
+from DB_Utils import DB_Utils
 
 class Database:
     def __init__(self, name):
         self.name = name
-        self.tables = []
+        self.tables : List[Table] = []
         self.create()
 
     def create(self):
-        DB_Utils.create_database(self.name)
-
+        try:
+            DB_Utils.create_database(self.name)
+        except FileExistsError:
+            print(f"La base de datos '{self.name}' ya existe. Cargando la base de datos")
+            self.load()
+    
+    def load(self):
+        table_names = DB_Utils.get_tables(self.name)
+        for table_name in table_names:
+            schema = DB_Utils.get_schema(self.name, table_name)
+            auto_increment = DB_Utils.get_auto_increment(self.name, table_name)
+            table = Table(self.name, table_name, schema, auto_increment, load=False)
+            self.tables.append(table)
+    
     def create_table(self, table_name, schema, auto_increment=False):
         table = Table(self.name, table_name, schema, auto_increment)
         return table
@@ -17,6 +30,9 @@ class Database:
         DB_Utils.delete_table(self.name, table_name)
     
     def drop(self):
+        table_names = [table.name for table in self.tables]
+        for table_name in table_names:
+            self.drop_table(table_name)
         DB_Utils.delete_database(self.name)
 
     def __str__(self) -> str:
@@ -24,28 +40,29 @@ class Database:
         return f"Database: {self.name}\n--------------------------\nTables: {tables_str}"
 
 class Table:
-    def __init__(self, DB_Name, name, schema, auto_increment=False):
+    def __init__(self, DB_Name, name, schema, auto_increment=False, load=True):
         self.name = name
         self.schema = schema
         self.auto_increment = auto_increment
         self.DB_Name = DB_Name
 
-        self._create(self, DB_Name)
+        if load:    
+            self._create()
 
-    def _create(self, DB_Name):
-        DB_Utils.create_table(DB_Name, self.name, self.schema, self.auto_increment)
+    def _create(self):
+        DB_Utils.create_table(self.DB_Name, self.name, self.schema, self.auto_increment)
 
-    def insert(self, DB_Name, data):
-        DB_Utils.insert_data(DB_Name, self.name, data)
+    def insert(self, data):
+        DB_Utils.insert_data(self.DB_Name, self.name, data)
 
-    def get(self, DB_Name, criteria=None):
-        return DB_Utils.get_data(DB_Name, self.name, criteria)
+    def get(self, criteria=None):
+        return DB_Utils.get_data(self.DB_Name, self.name, criteria)
 
-    def update(self, DB_Name, id_registro, new_data):
-        DB_Utils.update_data(DB_Name, self.name, id_registro, new_data)
+    def update(self, id_registro, new_data):
+        DB_Utils.update_data(self.DB_Name, self.name, id_registro, new_data)
 
-    def delete(self, DB_Name, id_registro):
-        DB_Utils.delete_data(DB_Name, self.name, id_registro)
+    def delete(self, id_registro):
+        DB_Utils.delete_data(self.DB_Name, self.name, id_registro)
 
     def __str__(self) -> str:
         return f"Table: {self.name}\n--------------------------\nSchema: {self.schema}\nAuto Increment: {self.auto_increment}"
--- src/back/DB/DB_Utils.py
+++ src/back/DB/DB_Utils.py
@@ -1,29 +1,36 @@
 import json
 import os
 
-from back.DB.Criteria import ICriteria
+from Criteria import ICriteria
 
 class DB_Utils:
-    def create_database(DB_Name):
+    
+    @staticmethod
+    def create_database(DB_Name, address):
         """Crea el directorio principal (base de datos) si no existe."""
+        db_path = os.path.join(address, DB_Name)
         try:
-            os.makedirs(DB_Name)
+            os.mkdir(db_path)
             print(f"Base de datos '{DB_Name}' creada.")
         except FileExistsError:
-            print(f"La base de datos '{DB_Name}' ya existe.")
-
-    def delete_database(DB_Name):
+            # print(f"La base de datos '{DB_Name}' ya existe.")
+            raise FileExistsError
+    
+    @staticmethod
+    def delete_database(DB_Name, address):
         """Elimina el directorio principal (base de datos) y su contenido."""
+        db_path = os.path.join(address, DB_Name)
         try:
-            os.rmdir(DB_Name)
+            os.rmdir(db_path)
             print(f"Base de datos '{DB_Name}' eliminada.")
         except FileNotFoundError: 
             print(f"La base de datos '{DB_Name}' no existe.")
 
-    def create_table(DB_Name, Table_Name, Schema, auto_increment=False):
+    @staticmethod
+    def create_table(DB_Name, Table_Name, Schema, address, auto_increment=False):
         """Crea un archivo JSON vacío para la tabla y un archivo .meta para almacenar información adicional."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
-        ruta_meta = os.path.join(DB_Name, f"{Table_Name}.meta")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
+        ruta_meta = os.path.join(address, DB_Name, f"{Table_Name}.meta")
 
         if not os.path.exists(ruta_archivo):
             with open(ruta_archivo, 'w') as archivo:
@@ -35,27 +42,58 @@ class DB_Utils:
                 json.dump(meta_info, archivo_meta, indent=4)  # Crea un archivo .meta con información adicional
                 print(f"Archivo .meta para la tabla '{Table_Name}' creado.")
             
-            ruta_esquema = os.path.join(DB_Name, f"{Table_Name}.schema.json")
+            ruta_esquema = os.path.join(address, DB_Name, f"{Table_Name}.schema.json")
             with open(ruta_esquema, 'w') as archivo_esquema:
                 json.dump(Schema, archivo_esquema, indent=4)
                 print(f"Esquema de la tabla '{Table_Name}' creado.")
         else:
             print(f"La tabla '{Table_Name}' ya existe.")
 
-    def delete_table(DB_Name, Table_Name):
+    @staticmethod
+    def delete_table(DB_Name, Table_Name, address):
         """Elimina el archivo JSON de la tabla."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
+        ruta_meta = os.path.join(address, DB_Name, f"{Table_Name}.meta")
+        ruta_esquema = os.path.join(address, DB_Name, f"{Table_Name}.schema.json")
         try:
             os.remove(ruta_archivo)
+            os.remove(ruta_meta)
+            os.remove(ruta_esquema)
             print(f"Tabla '{Table_Name}' eliminada.")
         except FileNotFoundError:
             print(f"La tabla '{Table_Name}' no existe.")
+    
+    @staticmethod
+    def get_tables(DB_Name: str, address: str):
+        """Devuelve una lista con los nombres de las tablas en la base de datos."""
+        db_path = os.path.join(address, DB_Name)
+        tables = [filename.split('.')[0] for filename in os.listdir(db_path) if filename.endswith('.json')]
+        return tables
 
-    def insert_data(DB_Name, Table_Name, data):
+    @staticmethod
+    def get_schema(DB_Name: str, table_name: str, address: str):
+        """Retorna el esquema de la tabla especificada por 'table_name' en la base de datos especificada por 'DB_Name'."""
+        schema_path = os.path.join(address, DB_Name, f"{table_name}.schema.json")
+        with open(schema_path, 'r') as schema_file:
+            schema = json.load(schema_file)
+        return schema
+
+    @staticmethod
+    def get_auto_increment(DB_Name: str, table_name: str, address: str):
+        """Retorna un booleano indicando si la tabla tiene autoincremento."""
+        meta_path = os.path.join(address, DB_Name, f"{table_name}.meta")
+        if os.path.exists(meta_path):
+            with open(meta_path, 'r') as meta_file:
+                meta_info = json.load(meta_file)
+                return meta_info.get("auto_increment", False)
+        return False
+
+    @staticmethod
+    def insert_data(DB_Name, Table_Name, data, address):
         """Agrega un nuevo objeto JSON al archivo de la tabla."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
-        ruta_esquema = os.path.join(DB_Name, f"{Table_Name}.schema.json")
-        ruta_meta = os.path.join(DB_Name, f"{Table_Name}.meta")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
+        ruta_esquema = os.path.join(address, DB_Name, f"{Table_Name}.schema.json")
+        ruta_meta = os.path.join(address, DB_Name, f"{Table_Name}.meta")
 
         # Verifica si la tabla tiene autoincremento
         if os.path.exists(ruta_meta):
@@ -69,13 +107,11 @@ class DB_Utils:
         if auto_increment:
             if "id" in data:
                 print("ADVERTENCIA: La tabla tiene autoincremento, no es necesario añadir el campo 'id' a los datos.")
-            else:
-                print("ADVERTENCIA: La tabla tiene autoincremento, se ignorará el campo 'id' proporcionado.")
 
         # Valida el tipo de dato usando el esquema
         with open(ruta_esquema, 'r') as archivo_esquema:
             esquema = json.load(archivo_esquema)
-            for columna, tipo_dato in esquema["columnas"].items():
+            for columna, tipo_dato in esquema.items():
                 if columna in data:
                     # Convierte el string del tipo de dato a un tipo real usando eval()
                     if not isinstance(data[columna], eval(tipo_dato)):
@@ -91,15 +127,20 @@ class DB_Utils:
                 data["id"] = last_index + 1
                 meta_info["last_index"] = data["id"]
 
+                with open(ruta_meta, 'r+') as archivo_meta:
+                    archivo_meta.seek(0)
+                    json.dump(meta_info, archivo_meta, indent=4)
+
             registros.append(data)
             archivo.seek(0)
             json.dump(registros, archivo, indent=4)
             print(f"Datos insertados en la tabla '{Table_Name}'.")
 
 
-    def get_data(DB_Name, Table_Name, criteria : ICriteria = None):
+    @staticmethod
+    def get_data(DB_Name, Table_Name, address, criteria : ICriteria):
         """Lee y filtra los datos del archivo JSON según el criterio (opcional)."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
         with open(ruta_archivo, 'r') as archivo:
             registros = json.load(archivo)
             if criteria:
@@ -109,9 +150,10 @@ class DB_Utils:
                 registros_filtrados = registros
             return registros_filtrados
 
-    def update_data(DB_Name, Table_Name, id_registro, new_data):
+    @staticmethod
+    def update_data(DB_Name, Table_Name, id_registro, new_data, address):
         """Actualiza un registro existente por su ID."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
         with open(ruta_archivo, 'r+') as archivo:
             registros = json.load(archivo)
             indice_registro = None
@@ -127,9 +169,10 @@ class DB_Utils:
             else:
                 print(f"Registro con ID {id_registro} no encontrado.")
 
-    def delete_data(DB_Name, Table_Name, id_registro):
+    @staticmethod
+    def delete_data(DB_Name, Table_Name, id_registro, address):
         """Elimina un registro existente por su ID."""
-        ruta_archivo = os.path.join(DB_Name, f"{Table_Name}.json")
+        ruta_archivo = os.path.join(address, DB_Name, f"{Table_Name}.json")
         with open(ruta_archivo, 'r+') as archivo:
             registros = json.load(archivo)
             registros = [registro for registro in registros if registro.get('id') != id_registro]
