5645a70491c64d3208c82245434c24426e302156
Loitzel <loitzelmorales@gmail.com>
2024-07-10 22:57:11 -0400

    protocol and rdcudp update

--- src/kademlia/protocol.py
+++ src/kademlia/protocol.py
@@ -1,61 +1,172 @@
+import random
+import asyncio
 import logging
 
 from kademlia.rpcudp import RPCProtocol
 
-log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+from kademlia.node import Node
+from kademlia.routing import RoutingTable
+from kademlia.utils import digest
+
+log = logging.getLogger(__name__)  
 
 
 class KademliaProtocol(RPCProtocol):
     def __init__(self, source_node, storage, ksize):
-               raise NotImplementedError
-
+        RPCProtocol.__init__(self)
+        self.router = RoutingTable(self, ksize, source_node)
+        self.storage = storage
+        self.source_node = source_node
 
     def get_refresh_ids(self):
-               raise NotImplementedError
+        """
+        Get ids to search for to keep old buckets up to date.
+        """
+        ids = []
+        
+        log.debug("LONELY BUCKETS: %s", self.router.lonely_buckets())
+        log.debug("BUCKETS: %s", self.router.buckets)
+        
+        for bucket in self.router.lonely_buckets():
+            rid = random.randint(*bucket.range).to_bytes(20, byteorder='big')
+            ids.append(rid)
+        
+        log.debug('GET REFRESH IDS: %s', ids)
+        
+        return ids
 
-
-    def rpc_stun(self, sender):  # pylint: disable=no-self-use
-               raise NotImplementedError
+    def rpc_stun(self, sender):  
+        return sender
 
     def rpc_ping(self, sender, nodeid):
-              raise NotImplementedError
+        source = Node(nodeid, sender[0], sender[1])
+        self.welcome_if_new(source)
+        
+        return self.source_node.id
 
     def rpc_refresh(self, sender, nodeid, key, value):
-               raise NotImplementedError
+        source = Node(nodeid, sender[0], sender[1])
+        self.welcome_if_new(source)
 
+        log.debug("got a refresh store request from %s, storing '%s'='%s'",
+                sender, key, value)
+        self.storage.set(key, value)
+        return True
 
     def rpc_store(self, sender, nodeid, key, value):
-               raise NotImplementedError
+        source = Node(nodeid, sender[0], sender[1])
+        self.welcome_if_new(source)
 
+        log.debug("got a store request from %s, storing '%s'='%s'",
+                sender, key, value)
+        self.storage[key] = value
+        
+        return True
 
     def rpc_find_node(self, sender, nodeid, key):
-               raise NotImplementedError
-
+        
+        log.info("finding neighbors of %i in local table",
+                int(nodeid, 16))
+        source = Node(nodeid, sender[0], sender[1])
+        self.welcome_if_new(source)
+        node = Node(key)
+        neighbors = self.router.find_neighbors(node, exclude=source)
+    
+        return list(map(tuple, neighbors))
 
     def rpc_find_value(self, sender, nodeid, key):
-               raise NotImplementedError
-
+        
+        source = Node(nodeid, sender[0], sender[1])
+        self.welcome_if_new(source)
+        value = self.storage.get(key, None)
+        if value is None:
+            return self.rpc_find_node(sender, nodeid, key)
+        
+        return {'value': value}
 
     async def call_find_node(self, node_to_ask, node_to_find):
-               raise NotImplementedError
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.find_node(address, self.source_node.id,
+                                    node_to_find.id)
+        return self.handle_call_response(result, node_to_ask)
 
     async def call_find_value(self, node_to_ask, node_to_find):
-               raise NotImplementedError
-
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.find_value(address, self.source_node.id,
+                                    node_to_find.id)
+        
+        return self.handle_call_response(result, node_to_ask)
 
     async def call_ping(self, node_to_ask):
-               raise NotImplementedError
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.ping(address, self.source_node.id)
+        
+        return self.handle_call_response(result, node_to_ask)
 
     async def call_store(self, node_to_ask, key, value):
-               raise NotImplementedError
-
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.store(address, self.source_node.id, key, value)
+        
+        return self.handle_call_response(result, node_to_ask)
+    
     async def call_refresh(self, node_to_ask, key, value):
-               raise NotImplementedError
-
+        address = (node_to_ask.ip, node_to_ask.port)
+        result = await self.refresh(address, self.source_node.id, key, value)
+        
+        return self.handle_call_response(result, node_to_ask)
 
     def welcome_if_new(self, node):
-               raise NotImplementedError
+        log.info("node %s", node)
+        """
+        Given a new node, send it all the keys/values it should be storing,
+        then add it to the routing table.
+
+        @param node: A new node that just joined (or that we just found out
+        about).
+
+        Process:
+        For each key in storage, get k closest nodes.  If newnode is closer
+        than the furtherst in that list, and the node for this server
+        is closer than the closest in that list, then store the key/value
+        on the new node (per section 2.5 of the paper)
+        """
+        
+        if not self.router.is_new_node(node):
+            log.info("already in router")
+            return
+
+        log.info("never seen %s before, adding to router", node)
+        log.debug("%s elements in storage of node %s", len(self.storage), self.source_node.long_id)
+        for key, value in self.storage:
+            
+            log.info("Element in storage: %s %s", key, value)
+            keynode = Node(digest(key))
+            neighbors = self.router.find_neighbors(keynode)
+            log.info("NEIGHBOURS %s", neighbors)
+            
+            if neighbors:
+                last = neighbors[-1].distance_to(keynode)
+                new_node_close = node.distance_to(keynode) < last
+                log.info("NEW NODE CLOSE %s", new_node_close)
+                first = neighbors[0].distance_to(keynode)
+                this_closest = self.source_node.distance_to(keynode) < first
+                log.info("THIS CLOSEST %s", this_closest)
+            
+            if not neighbors or (new_node_close and this_closest):
+                asyncio.ensure_future(self.call_store(node, key, value))
+        
+        self.router.add_contact(node)
 
     def handle_call_response(self, result, node):
-               raise NotImplementedError
+        """
+        If we get a response, add the node to the routing table.  If
+        we get no response, make sure it's removed from the routing table.
+        """
+        if not result[0]:
+            log.warning("no response from %s, removing from router", node)
+            self.router.remove_contact(node)
+            return result
 
+        log.info("got successful response from %s", node)
+        self.welcome_if_new(node)
+        return result
