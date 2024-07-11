60ad06b518c45b02837981af5ace1986411e9664
Loitzel <loitzelmorales@gmail.com>
2024-07-11 00:08:34 -0400

    network

--- src/kademlia/network.py
+++ src/kademlia/network.py
@@ -1,16 +1,22 @@
 """
 Package for interacting on the network at a high level.
 """
-
+import random
+import pickle
+import asyncio
 import logging
+import uuid
 
 from kademlia.protocol import KademliaProtocol
+from kademlia.utils import digest
+from kademlia.storage import Storage, ForgetfulStorage
+from kademlia.node import Node
+from kademlia.crawling import ValueSpiderCrawl
+from kademlia.crawling import NodeSpiderCrawl
+
+log = logging.getLogger(__name__)  
 
 
-log = logging.getLogger(__name__) 
-
-
-# pylint: disable=too-many-instance-attributes
 class Server:
     """
     High level view of a node instance.  This is the object that should be
@@ -20,68 +26,276 @@ class Server:
     protocol_class = KademliaProtocol
 
     def __init__(self, ip, ksize=3, alpha=2, node_id=None, storage=None):
-        raise NotImplementedError
+        """
+        Create a server instance.  This will start listening on the given port.
 
+        Args:
+            ksize (int): The k parameter from the paper
+            alpha (int): The alpha parameter from the paper
+            node_id: The id for this node on the network.
+            storage: An instance that implements the interface
+                     :class:`~kademlia.storage.IStorage`
+        """
+        self.id = uuid.uuid4()
+        self.ksize = ksize
+        self.alpha = alpha
+        self.storage = Storage(self.id)
+        self.node = Node(node_id or digest(ip))
+        self.transport = None
+        self.protocol = None
+        self.refresh_loop = None
+        self.save_state_loop = None
 
     def stop(self):
-        raise NotImplementedError
+        if self.transport is not None:
+            self.transport.close()
 
+        if self.refresh_loop:
+            self.refresh_loop.cancel()
+
+        if self.save_state_loop:
+            self.save_state_loop.cancel()
 
     def _create_protocol(self):
-        raise NotImplementedError
-
+        return self.protocol_class(self.node, self.storage, self.ksize)
 
     async def listen(self, port, interface='0.0.0.0'):
-        raise NotImplementedError
+        """
+        Start listening on the given port.
 
+        Provide interface="::" to accept ipv6 address
+        """
+        loop = asyncio.get_event_loop()
+        listen = loop.create_datagram_endpoint(self._create_protocol,
+                                               local_addr=(interface, port))
+        log.info("Node %i listening on %s:%i",
+                 self.node.long_id, interface, port)
+        self.node.ip = interface
+        self.node.port = port
+        self.transport, self.protocol = await listen
+        self.refresh_table()
 
     def refresh_table(self):
-        raise NotImplementedError
+        log.debug("Refreshing routing table")
+        asyncio.ensure_future(self._refresh_table())
+        loop = asyncio.get_event_loop()
+        self.refresh_loop = loop.call_later(30, self.refresh_table)
 
     async def _refresh_table(self):
-        raise NotImplementedError
+        """
+        Refresh buckets that haven't had any lookups in the last hour
+        (per section 2.3 of the paper).
+        """
+        results = []
+        for node_id in self.protocol.get_refresh_ids():
+            node = Node(node_id)
+            nearest = self.protocol.router.find_neighbors(node, self.alpha)
+            log.debug('NEAREST IN REFRESH TABLE: %s', nearest)
+            spider = NodeSpiderCrawl(self.protocol, node, nearest,
+                                     self.ksize, self.alpha)
+            results.append(spider.find())
 
+        await asyncio.gather(*results)
+
+        for dkey, value in self.storage:
+            data = self.storage.get(dkey)
+            await self.set_refresh(dkey, data)
 
     def bootstrappable_neighbors(self):
-        raise NotImplementedError
+        """
+        Get a :class:`list` of (ip, port) :class:`tuple` pairs suitable for
+        use as an argument to the bootstrap method.
 
+        The server should have been bootstrapped
+        already - this is just a utility for getting some neighbors and then
+        storing them if this server is going down for a while.  When it comes
+        back up, the list of nodes can be used to bootstrap.
+        """
+        neighbors = self.protocol.router.find_neighbors(self.node)
+        return [tuple(n)[-2:] for n in neighbors]
 
     async def bootstrap(self, addrs):
-        raise NotImplementedError
+        """
+        Bootstrap the server by connecting to other known nodes in the network.
 
+        Args:
+            addrs: A `list` of (ip, port) `tuple` pairs.  Note that only IP
+                   addresses are acceptable - hostnames will cause an error.
+        """
+        log.debug("Attempting to bootstrap node with %i initial contacts",
+                  len(addrs))
+        cos = list(map(self.bootstrap_node, addrs))
+        gathered = await asyncio.gather(*cos)
+        nodes = [node for node in gathered if node is not None]
+        spider = NodeSpiderCrawl(self.protocol, self.node, nodes,
+                                 self.ksize, self.alpha)
+        return await spider.find()
 
     async def bootstrap_node(self, addr):
-        raise NotImplementedError
-
+        result = await self.protocol.ping(addr, self.node.id)
+        return Node(result[1], addr[0], addr[1]) if result[0] else None
 
     async def get(self, key, refresh=False):
-       raise NotImplementedError
+        """
+        Get a key if the network has it.
 
+        Returns:
+            :class:`None` if not found, the value otherwise.
+        """
+        log.info("Looking up key %s", key)
+        dkey = key
+        if not refresh:
+            dkey = digest(key)
+        res_self = self.storage.get(dkey)
+        log.debug("RESULT GET SELF: %s", res_self)
+
+        node = Node(dkey)
+        nearest = self.protocol.router.find_neighbors(node)
+        if not nearest:
+            log.warning("There are no known neighbors to get key %s", key)
+            return None
+        spider = ValueSpiderCrawl(self.protocol, node, nearest,
+                                  self.ksize, self.alpha)
+        result = await spider.find()
+
+        log.debug("RESULT GET: %s", result)
+
+        if res_self is not None and result is not None:
+            if res_self[0] > result[0]:
+                return res_self[1] 
+            else:
+                self.storage[dkey] = result[1]
+                return result[1]
+        if res_self is not None:
+            return res_self[1]
+        if result is not None:
+            return result[1]
+        return None
 
     async def set(self, key, value):
-       raise NotImplementedError
-
+        """
+        Set the given string key to the given value in the network.
+        """
+        if not check_dht_value_type(value):
+            raise TypeError(
+                "Value must be of type int, float, bool, str, or bytes"
+            )
+        log.info("setting '%s' = '%s' on network", key, value)
+        dkey = digest(key)
+        return await self.set_digest(dkey, value)
 
     async def set_digest(self, dkey, value):
-       raise NotImplementedError
+        """
+        Set the given SHA1 digest key (bytes) to the given value in the
+        network.
+        """
+        node = Node(dkey)
 
+        nearest = self.protocol.router.find_neighbors(node)
+
+        if not nearest:
+            log.warning("There are no known neighbors to set key %s",
+                        dkey)
+            return False
+
+        spider = NodeSpiderCrawl(self.protocol, node, nearest,
+                                    self.ksize, self.alpha)
+        nodes = await spider.find()
+        log.info("setting '%s' on %s", dkey, list(map(str, nodes)))
+        log.debug("NODES SET DIGEST %s",nodes)
+
+        biggest = max([n.distance_to(node) for n in nodes])
+        if self.node.distance_to(node) < biggest:
+            self.storage[dkey] = value
+        results = [self.protocol.call_store(n, dkey, value) for n in nodes]
+        return any(await asyncio.gather(*results))
 
     async def set_refresh(self, dkey, value):
-        raise NotImplementedError
+        """
+        Set the given SHA1 digest key (bytes) to the given value in the
+        network.
+        """
+        node = Node(dkey)
 
+        nearest = self.protocol.router.find_neighbors(node)
+        
+        if not nearest:
+            log.warning("There are no known neighbors to set key %s",
+                        dkey)
+            return False
+
+        spider = NodeSpiderCrawl(self.protocol, node, nearest,
+                                self.ksize, self.alpha)
+        nodes = await spider.find()
+        log.info("setting '%s' on %s", dkey, list(map(str, nodes)))
+        log.debug("NODES SET DIGEST %s",nodes)
+
+        results = [self.protocol.call_refresh(n, dkey, value) for n in nodes]
+        return any(await asyncio.gather(*results))
 
     def save_state(self, fname):
-        raise NotImplementedError
-
+        """
+        Save the state of this node (the alpha/ksize/id/immediate neighbors)
+        to a cache file with the given fname.
+        """
+        log.info("Saving state to %s", fname)
+        data = {
+            'ksize': self.ksize,
+            'alpha': self.alpha,
+            'id': self.node.id,
+            'neighbors': self.bootstrappable_neighbors()
+        }
+        if not data['neighbors']:
+            log.warning("No known neighbors, so not writing to cache.")
+            return
+        with open(fname, 'wb') as file:
+            pickle.dump(data, file)
 
     @classmethod
     async def load_state(cls, fname, port, interface='0.0.0.0'):
-       raise NotImplementedError
-
+        """
+        Load the state of this node (the alpha/ksize/id/immediate neighbors)
+        from a cache file with the given fname and then bootstrap the node
+        (using the given port/interface to start listening/bootstrapping).
+        """
+        log.info("Loading state from %s", fname)
+        with open(fname, 'rb') as file:
+            data = pickle.load(file)
+        svr = cls(data['ksize'], data['alpha'], data['id'])
+        await svr.listen(port, interface)
+        if data['neighbors']:
+            await svr.bootstrap(data['neighbors'])
+        return svr
 
     def save_state_regularly(self, fname, frequency=600):
-        raise NotImplementedError
+        """
+        Save the state of node with a given regularity to the given
+        filename.
+
+        Args:
+            fname: File name to save retularly to
+            frequency: Frequency in seconds that the state should be saved.
+                        By default, 10 minutes.
+        """
+        self.save_state(fname)
+        loop = asyncio.get_event_loop()
+        self.save_state_loop = loop.call_later(frequency,
+                                                self.save_state_regularly,
+                                                fname,
+                                                frequency)
 
 
 def check_dht_value_type(value):
-   raise NotImplementedError
+    """
+    Checks to see if the type of the value is a valid type for
+    placing in the dht.
+    """
+    typeset = [
+        int,
+        float,
+        bool,
+        str,
+        bytes,
+        dict
+    ]
+    return type(value) in typeset 
--- src/kademlia/storage.py
+++ src/kademlia/storage.py
@@ -1,8 +1,14 @@
 import logging
+import time
+from itertools import takewhile
+import operator
+from collections import OrderedDict
+from abc import abstractmethod, ABC
+import dictdatabase as DDB
+import json
+import pickle
 
-
-
-log = logging.getLogger(__name__) 
+log = logging.getLogger(__name__)  
 
 class Storage:
     """
@@ -11,64 +17,144 @@ class Storage:
     """
 
     def __init__(self, file_name, ttl=604800):
-        raise NotImplementedError
+        self.file_name = file_name
+        self.ttl = ttl
+
+        DDB.config.storage_directory = "database"
+
+        database = DDB.at(f"{self.file_name}")
+        if not database.exists():
+            database.create({})
+        else:
+            #database already exist, do nothing
+            pass
 
     def __setitem__(self, key, value):
-        raise NotImplementedError
+        """
+        Set a key to the given value.
+        """
+        if value == None:
+            pass
+        else:
+            with DDB.at(f"{self.file_name}").session() as (session, file):
+                file[f"{key}"] = (time.monotonic(), value)
+                session.write()
 
     def cull(self):
-        raise NotImplementedError
+        with DDB.at(f"{self.file_name}").session() as (session, data):
+            print(self.iter_older_than(self.ttl))
+            for id, _ in self.iter_older_than(self.ttl):
+                del data[id]
+            session.write()
 
     def __getitem__(self, key):
-        raise NotImplementedError
+        """
+        Get the given key.  If item doesn't exist, raises C{KeyError}
+        """
+        if DDB.at(f"{self.file_name}", key=f"{key}").exists():
+            return DDB.at(f"{self.file_name}", key=f"{key}").read()[1]
 
     def set(self, key, value):
-        raise NotImplementedError
+        if value == None:
+            pass
+        else:
+            data = value
+            log.debug("New data %s", data)
+            if DDB.at(f"{self.file_name}", key=key).exists():
+                data_read = DDB.at(f"{self.file_name}", key=f"{key}").read()
+                log.debug("Data %s", data_read)
+                if value[0] < data_read[0]:
+                    data =data_read                  
+
+            with DDB.at(f"{self.file_name}").session() as (session, file):
+                file[f"{key}"] = data
+                session.write()
 
     def get(self, key: str, default=None):
-        raise NotImplementedError
+        """
+        Get given key.  If not found, return default.
+        """
+
+        if DDB.at(f"{self.file_name}", key=key).exists():
+            log.debug("DDB EXISTS!!!!!!!!!!!!!!!!")
+            return DDB.at(f"{self.file_name}", key=f"{key}").read()
+        log.debug("DDB DOES NOT EXISTS!!!!!!!!!!!!!!!!")
+        return default
     
     def iter_older_than(self, seconds_old):
-        raise NotImplementedError
+        min_birthday = time.monotonic() - seconds_old
+        zipped = self._triple_iter()
+        matches = takewhile(lambda r: min_birthday >= r[1], zipped)
+        return list(map(operator.itemgetter(0, 2), matches))
 
     def _triple_iter(self):
-        raise NotImplementedError
+        data = DDB.at(f"{self.file_name}").read()
+        keys = data.keys()
+        birthday = map(operator.itemgetter(0), data.values())
+        values = map(operator.itemgetter(1), data.values())
+        return zip(keys, birthday, values)
 
     def __iter__(self):
-        raise NotImplementedError
+        data = DDB.at(f"{self.file_name}").read()
+        keys = data.keys()
+        values = map(operator.itemgetter(1), data.values())
+        return zip(keys, values)
 
     def __repr__(self):
-        raise NotImplementedError
+        return repr(DDB.at(f"{self.file_name}").read())
     
     def __len__(self):
-        raise NotImplementedError
+        data = DDB.at(f"{self.file_name}").read()
+        return len(data)
 
 class ForgetfulStorage(Storage):
     def __init__(self, ttl=604800):
-        raise NotImplementedError
+        """
+        By default, max age is a week.
+        """
+        self.data = OrderedDict()
+        self.ttl = ttl
 
     def __setitem__(self, key, value):
-        raise NotImplementedError
-    
+        if key in self.data:
+            del self.data[key]
+        self.data[key] = (time.monotonic(), value)
+        self.cull()
+
     def cull(self):
-        raise NotImplementedError
+        for _, _ in self.iter_older_than(self.ttl):
+            self.data.popitem(last=False)
 
     def get(self, key, default=None):
-        raise NotImplementedError
+        self.cull()
+        if key in self.data:
+            return self[key]
+        return default
 
     def __getitem__(self, key):
-        raise NotImplementedError
+        self.cull()
+        return self.data[key][1]
 
     def __repr__(self):
-        raise NotImplementedError
+        self.cull()
+        return repr(self.data)
 
     def iter_older_than(self, seconds_old):
-        raise NotImplementedError
+        min_birthday = time.monotonic() - seconds_old
+        zipped = self._triple_iter()
+        matches = takewhile(lambda r: min_birthday >= r[1], zipped)
+        return list(map(operator.itemgetter(0, 2), matches))
 
     def _triple_iter(self):
-        raise NotImplementedError
+        ikeys = self.data.keys()
+        ibirthday = map(operator.itemgetter(0), self.data.values())
+        ivalues = map(operator.itemgetter(1), self.data.values())
+        return zip(ikeys, ibirthday, ivalues)
 
     def __iter__(self):
-        raise NotImplementedError
+        self.cull()
+        ikeys = self.data.keys()
+        ivalues = map(operator.itemgetter(1), self.data.values())
+        return zip(ikeys, ivalues)
 
 
